7ebbb4e188332cf872ec7238bff9c7a23da7691d
Daniel <dany.pet@seznam.cz>
2020-11-10 09:18:56 +0100

    Druhy comit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <LedMatrix.h>
+ 
+#define NUMBER_OF_DEVICES 4
+#define CS_PIN 8
+LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
+ 
+void setup()
+{
+ledMatrix.init();
+ledMatrix.setIntensity(4); // range is 0-15
+ledMatrix.setText("Testovaci text");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void loop()
+{
+ledMatrix.clear();
+ledMatrix.scrollTextLeft();
+ledMatrix.drawText();
+ledMatrix.commit();
+delay(200);
 }
