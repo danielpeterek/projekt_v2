d2b35740b80aa5647407288dad6125b76709dbd6
danielpeterek <dany.pet@seznam.cz>
2020-11-11 15:02:49 +0100

    Test displeje

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <SPI.h>
-#include <LedMatrix.h>
+#include "LedMatrix.h"
  
 #define NUMBER_OF_DEVICES 4
-#define CS_PIN 8
+#define CS_PIN 2
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
  
 void setup()
 {
+Serial.begin(115200);
 ledMatrix.init();
 ledMatrix.setIntensity(4); // range is 0-15
 ledMatrix.setText("Testovaci text");
